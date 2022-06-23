f9eb25020c2bf240a7d14be7db72b3ee9fbc8971
mhzcee <105357275+mhzcee@users.noreply.github.com>
2022-06-23 18:45:37 -0500

    Javascript that prints my profile

---
+++ script.js
@@ -0,0 +1,5 @@
+const myName = "Comfort Okoi";
+const myHeight = "178 cm";
+const myCountry = "Nigeria";
+
+alert("My name is " + myName+ "\nMy height is " + myHeight + "\nMy country is " + myCountry)
